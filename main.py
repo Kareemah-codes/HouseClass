d713c13a5e59753bb0ff4febb89310c623ba57ff
Kareemah Ajina <>
2021-05-05 01:19:22 +0000

    Added the sell house method

--- main.py
+++ main.py
@@ -15,4 +15,17 @@ class House():
     self.weeks_on_mrket= 0
   
   def display_info(self):
+    """Display information on the house"""
+    print("\n ......House for Sale......")
     print(f"Style :{self.style.title()}\n Sq_footage : {self.sq_footage}\n Year Built : {self.year_built}\n Price : {self.price} \n Sold : {self.sold}\n Weeks on market : {self.weeks_on_mrket}" )
+
+  def sell_house(self):
+    """Sell the house"""
+   if self.sold == False :
+     print(f"Congrats! Your house sold for {self.price}")
+     self.sold == True
+    else:
+      print("Sorry,this house is no longer for sale")
+richards_house = House("bungalow",1200,1918,100000)
+
+richards_house.display_info()
